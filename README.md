2a4f6e4030bbc10cf5dd3e78f1dbc056078a8df1
Brent Garber <me@bgarber.work>
2023-11-15 14:32:17 -0500

    Improve code quality and best practices
    - Add comprehensive type hints throughout the codebase
    - Replace deprecated GitHub Actions ::set-output syntax with modern output format
    - Implement proper input validation with detailed error messages
    - Add comprehensive docstrings and function documentation
    - Improve error handling with specific exception types
    - Add missing maxtime input parameter to action.yml
    - Enhance README with detailed usage examples and documentation
    - Replace magic numbers with named constants
    - Add proper resource cleanup with try-finally blocks

--- README.md
+++ README.md
@@ -1,4 +1,67 @@
 # action-tcp-connection
-GitHub Action to make a TCP connection
+
+GitHub Action to make a TCP connection with retry logic and comprehensive error handling.
 
 [![Github Action - TCP Test](https://github.com/BGarber42/action-tcp-connection/actions/workflows/main.yml/badge.svg)](https://github.com/BGarber42/action-tcp-connection/actions/workflows/main.yml)
+
+## Features
+
+- TCP connection testing with automatic retry logic
+- Comprehensive input validation
+- Detailed error reporting
+- Configurable timeout settings
+- Support for custom content transmission
+
+## Usage
+
+### Basic Usage
+
+```yaml
+- name: Test TCP Connection
+  uses: BGarber42/action-tcp-connection@main
+  with:
+    remotehost: 'example.com'
+    remoteport: '80'
+```
+
+### Advanced Usage
+
+```yaml
+- name: Test TCP Connection with Custom Timeout
+  uses: BGarber42/action-tcp-connection@main
+  with:
+    remotehost: '192.168.1.100'
+    remoteport: '8080'
+    maxtime: '120'
+```
+
+## Inputs
+
+| Parameter | Description | Required | Default |
+|-----------|-------------|----------|---------|
+| `remotehost` | IP address or hostname to connect to | Yes | `127.0.0.1` |
+| `remoteport` | Port number to connect to (1-65535) | Yes | `80` |
+| `maxtime` | Maximum time in seconds to retry connection | No | `60` |
+
+## Outputs
+
+| Parameter | Description |
+|-----------|-------------|
+| `myOutput` | Success message when connection is established |
+| `error` | Error message if connection fails |
+
+## Error Handling
+
+The action includes comprehensive error handling for:
+- Invalid hostnames or IP addresses
+- Invalid port numbers
+- Connection timeouts
+- Network errors
+- Input validation errors
+
+## Best Practices
+
+1. Always specify the exact host and port you want to test
+2. Use appropriate timeout values for your network conditions
+3. Handle the error output in your workflow for better debugging
+4. Consider using this action in CI/CD pipelines to verify service availability
--- entrypoint.py
+++ entrypoint.py
@@ -1,40 +1,98 @@
 import os
 import socket
 import logging
+from typing import Optional
 
 import backoff
 
 
 @backoff.on_exception(
-    backoff.expo, ConnectionRefusedError, max_time=os.environ.get("INPUT_MAXTIME", 60)
+    backoff.expo, ConnectionRefusedError, max_time=int(os.environ.get("INPUT_MAXTIME", "60"))
 )
-def netcat(host, port, content=None):
+def netcat(host: str, port: int, content: Optional[str] = None) -> None:
+    """
+    Establish a TCP connection to the specified host and port.
+    
+    Args:
+        host: The target hostname or IP address
+        port: The target port number
+        content: Optional content to send after connection
+    """
+    BUFFER_SIZE = 4096
+    
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((host, int(port)))
-    if content:
-        s.sendall(content.encode())
-    s.shutdown(socket.SHUT_WR)
-    while True:
-        data = s.recv(4096)
-        if not data:
-            break
-        print(repr(data))
-    s.close()
-
-
-def main():
-    logging.getLogger("backoff").addHandler(logging.StreamHandler())
-    my_host = os.environ["INPUT_REMOTEHOST"]
-    my_port = os.environ["INPUT_REMOTEPORT"]
-
     try:
-        netcat(my_host, my_port)
-    except TimeoutError:
-        print("Timed out waiting for connection")
-        raise Exception("Timed out waiting for connection")
+        s.connect((host, port))
+        if content:
+            s.sendall(content.encode())
+        s.shutdown(socket.SHUT_WR)
+        
+        while True:
+            data = s.recv(BUFFER_SIZE)
+            if not data:
+                break
+            print(repr(data))
+    finally:
+        s.close()
 
-    my_output = "Yay!"
-    print(f"::set-output name=myOutput::{my_output}")
+
+def validate_inputs(host: str, port: str) -> tuple[str, int]:
+    """
+    Validate and convert input parameters.
+    
+    Args:
+        host: The hostname or IP address
+        port: The port number as string
+        
+    Returns:
+        Tuple of (host, port) with validated values
+        
+    Raises:
+        ValueError: If inputs are invalid
+    """
+    if not host or not host.strip():
+        raise ValueError("Host cannot be empty")
+    
+    try:
+        port_num = int(port)
+        if not (1 <= port_num <= 65535):
+            raise ValueError("Port must be between 1 and 65535")
+    except ValueError as e:
+        raise ValueError(f"Invalid port number: {port}") from e
+    
+    return host.strip(), port_num
+
+
+def main() -> None:
+    """Main function to handle TCP connection test."""
+    logging.getLogger("backoff").addHandler(logging.StreamHandler())
+    
+    try:
+        my_host = os.environ["INPUT_REMOTEHOST"]
+        my_port = os.environ["INPUT_REMOTEPORT"]
+        
+        # Validate inputs
+        host, port = validate_inputs(my_host, my_port)
+        
+        # Attempt connection
+        netcat(host, port)
+        
+        # Use new GitHub Actions output syntax
+        output_message = "Connection successful!"
+        print(f"myOutput={output_message}")
+        
+    except TimeoutError:
+        error_msg = "Timed out waiting for connection"
+        print(f"error={error_msg}")
+        raise Exception(error_msg)
+    except ValueError as e:
+        error_msg = f"Invalid input: {str(e)}"
+        print(f"error={error_msg}")
+        raise Exception(error_msg)
+    except Exception as e:
+        error_msg = f"Connection failed: {str(e)}"
+        print(f"error={error_msg}")
+        raise
 
 
 if __name__ == "__main__":
